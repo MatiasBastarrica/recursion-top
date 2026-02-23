eb4b4b0166309941d688e90727d541a720c82bfe
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-23 09:34:38 -0300

    Write the function fibs using iteration

---
+++ fibonacci.js
@@ -0,0 +1,16 @@
+console.log("hello");
+function fibs(num) {
+  let arr = [0, 1];
+  if (num === 1) {
+    return [0];
+  } else if (num === 2) {
+    return arr;
+  }
+  for (let i = 2; i < num; i++) {
+    let result = arr[i - 1] + arr[i - 2];
+    arr.push(result);
+  }
+  return arr;
+}
+
+console.log(fibs(8));
